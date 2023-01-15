cc256d6f2ec7634ce913678166d1484b7002b188
JayashreeSelvarangam <116268739+JayashreeSelvarangam@users.noreply.github.com>
2023-01-15 07:55:17 +0530

    changes

---
+++ pages/js/script.js
@@ -0,0 +1,43 @@
+// for (let i=0; i<100; i++) {
+// console.log("superstar");
+
+// }
+
+  // for (let count99 = 0; count99 < 100; count99++){
+   
+  //       console.log("superstar");
+ 
+  //   for (let count = 0; count < 2; count++){
+  //       console.log("rajinikanth");
+  //   }
+    
+  // }
+  // for(let i=1; i<=100; 1++){
+  //   if(i % 2 === 1){
+  //       console.log (`${i}`)
+  //   }
+  // }
+// const n=7;
+// for(  let i= 1;i <= n ;i++){
+//   console.log(i,i,i,i,i,i,i);
+// }
+// const n=7;
+// for(let x=1; x <=n; x++){
+//   let output=" ";
+//   for(let y=1; y<=n; y++){
+//     output+=x+" ";
+//   }
+// console.log(output);
+// }
+
+// const n = 5
+// for (let x = 1; x <= n; x++) {
+//     let output = "  ";
+//     for (let y = 1; y <= x; y++) {
+//         output += y + " ";
+//     }
+//     console.log(output);
+// }
+
+
+
